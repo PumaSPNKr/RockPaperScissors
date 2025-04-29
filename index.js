9cfb451b7417a45ebdad74e35717e68c3c7cb87b
Jordan <Jfma.din@gmail.com>
2025-04-29 16:40:22 -0400

    Add displayChoices function

--- index.js
+++ index.js
@@ -16,5 +16,29 @@ function getHumanChoice() {
     return humanChoice.toLowerCase();
 }
 
-console.log(getComputerChoice());
+function displayChoices(humanChoice, computerChoice) {
+    console.log ("You chose " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) +
+                 ". Computer chose " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+                  + ". ");
+}
+
+// function playRound(humanChoice, computerChoice) {
+//     if (humanChoice == "rock"){
+//         if (computerChoice == "paper") {
+//             displayChoices(humanChoice, computerChoice);
+
+//             computerScore++;
+//         } else if (computerChoice == "scissors") {
+//             console.log ("You chose " + humanChoice[0].toUpperCase() + ". Computer chose " +
+//             computerChoice[0].toUpperCase() + ". You lose!");
+//             computerScore++;
+//         }
+//     }
+
+// }
+
 let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+displayChoices(humanChoice, computerChoice);
+
+let humanScore, computerScore = 0;
